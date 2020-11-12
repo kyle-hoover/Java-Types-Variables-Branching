97a7c28943a356438fced369ac6107b39e17ec80
Kyle <kylehoover76@gmail.com>
2020-11-11 19:43:43 -0500

    Create JTVB.java

---
+++ JTVB.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    String x = 8;
+    System.out.println(x);
+    //Main.java:5: error: incompatible types: int cannot be converted to String
+
+    Int y = "Hello world!";
+    System.out.println(y);
+    //Main.java:8: error: cannot find symbol
+
+    Int z = 
+    System.out.println(z);
+    //Main.java:12: error: cannot find symbol
+
+    If (a > 5) {
+      System.out.println("Greater than 5!");
+    }
+    //Main.java:12: error: cannot find symbol
+  }
+}
